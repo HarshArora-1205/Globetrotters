9199c943842dc3ed9ec427022a899cf86eb5fbf4
Harsh Arora <iamharsharora1205@gmail.com>
2023-07-15 01:53:48 +0530

    Created basic CRUD operations & Modified Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import axios from 'axios';
 import Home from "./views/Home";
 import Escapes from "./views/escapes/Escapes";
+import Show from "./views/escapes/Show";
+import Edit from "./views/escapes/Edit";
+import New from "./views/escapes/New";
 
-
+axios.defaults.baseURL = "http://localhost:3001";
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/escapes" element={<Escapes />}/>
+        <Route path="/escapes/:id" element={<Show />}/>
+        <Route path="/escapes/:id/edit" element={<Edit />}/>
+        <Route path="/escapes/new" element={<New />}/>
       </Routes>
     </BrowserRouter>
   );
--- client/src/views/escapes/Escapes.jsx
+++ client/src/views/escapes/Escapes.jsx
@@ -5,19 +5,26 @@ const Escapes = () => {
   const [escapes, setEscapes] = useState([]);
   
   useEffect(() => {
-    axios.get("http://localhost:3001/escapes").then((res) => {
-      setEscapes(res.data.escapes);
+    axios
+      .get("/escapes")
+      .then((res) => {
+        setEscapes(res.data.escapes);
+      })
+      .catch((err) => {
+        console.log("Error in fetching Escapes: Escapes JSX");
+        console.log(err);
     });
   }, []);
 
   return (
     <>
       <h1>All Escapes</h1>
+      <a href="/escapes/new">New!</a>
       <ul>
         {escapes?.length ? (
-          escapes.map((escape, index) => <li key={index}>{escape.title}</li>)
+          escapes.map((escape, index) => <li key={index}><a href={`/escapes/${escape._id}`}>{escape.title}</a></li>)
         ) : (
-          <h2>No Escapes found!</h2>
+          <h2>No Escape found!</h2>
         )}
       </ul>
     </>
