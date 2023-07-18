35b564ffa5967ebe840d8804b03dcaa9d306a1de
Harsh Arora <iamharsharora1205@gmail.com>
2023-07-18 13:56:47 +0530

    Restructured Escape Routes

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 import Escape from "./models/Escape.js";
 import Review from "./models/Review.js";
 import cors from "cors";
-import catchAsync from "./utils/catchAsync.js";
 import ExpressError from "./utils/ExpressError.js";
-import { escapeSchema, reviewSchema } from "./joiSchemas.js";
+import { reviewSchema } from "./joiSchemas.js";
+import escapes from "./routes/escapes.js"
 
 mongoose.connect('mongodb://0.0.0.0:27017/globetrotters');
 const db = mongoose.connection;
@@ -28,17 +28,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+app.use("/escapes", escapes);
 
-const validateEscape = (req, res, next) => {
-    const {error} = escapeSchema.validate(req.body)
-    if(error){
-        const msg = error.details.map((el) => el.message).join(',');
-        throw new ExpressError(msg, 400);
-    }
-    else{
-        next();
-    }
-}
 
 const validateReview = (req, res, next) => {
     const {error} = reviewSchema.validate(req.body);
@@ -51,36 +42,7 @@ const validateReview = (req, res, next) => {
     }
 }
  
-app.get('/escapes', catchAsync(async(req,res) => {
-    const escapes = await Escape.find({});
-    res.send({escapes});
-}))
 
-app.post('/escapes/new', validateEscape, catchAsync(async (req, res) => {
-    const escape = new Escape(req.body.escape);
-    await escape.save();
-    res.send(escape._id);
-}));
-
-app.get('/escapes/:id', catchAsync(async (req,res) => {
-    const escape = await Escape.findById(req.params.id).populate("reviews");
-    res.send({escape});
-}))
-
-app.put('/escapes/:id', validateEscape, catchAsync(async (req, res) => {
-    const escape = await Escape.findByIdAndUpdate(req.params.id, {...req.body.escape});
-    res.send(escape._id);
-}))
-
-app.delete('/escapes/:id', catchAsync(async (req,res) => {
-    const escape = await Escape.findByIdAndDelete(req.params.id);
-    if(escape){
-        res.status(200).send("Escape Deleted Successfully");
-    }
-    else{
-        res.status(404).send("Escape Not Found")
-    }
-}))
 
 app.post('/escapes/:id/reviews', validateReview, async (req,res) => {
     const escape = await Escape.findById(req.params.id);
